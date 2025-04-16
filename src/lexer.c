707e59a04a940e0b8ad2a40c53e48ed84c2f018e
nicyoong <nicyoong321@gmail.com>
2025-04-17 00:18:21 +0800

    Chore: move struct definitions
    move struct definitions from lexer.c to parser.h

--- src/lexer.c
+++ src/lexer.c
@@ -1,35 +1,5 @@
 
 
-// Structs
-typedef enum e_token_type {
-    TOKEN_WORD,
-    TOKEN_PIPE,
-    TOKEN_REDIRECT_IN,
-    TOKEN_REDIRECT_OUT,
-    TOKEN_REDIRECT_APPEND,
-    TOKEN_REDIRECT_HEREDOC
-} t_token_type;
-
-typedef enum e_segment_type {
-    LITERAL,
-    VARIABLE,
-    VAR_EXIT_STATUS
-} t_segment_type;
-
-typedef struct s_segment {
-    t_segment_type type;
-    char *value;
-} t_segment;
-
-typedef struct s_word {
-    t_list *segments;
-} t_word;
-
-typedef struct s_token {
-    t_token_type type;
-    t_word *word;
-} t_token;
-
 // ==============================
 // Helper Functions
 // ==============================
