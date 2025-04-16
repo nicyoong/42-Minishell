5b25e86fbf488f9dcb19bb663ed6ff373e2b975d
nicyoong <nicyoong321@gmail.com>
2025-04-17 00:37:46 +0800

    chore: fix structs

--- src/parser.h
+++ src/parser.h
@@ -8,55 +8,54 @@
 # include "../libft/libft.h"
 
 typedef enum e_token_type {
-    TOKEN_WORD,
-    TOKEN_PIPE,
-    TOKEN_REDIRECT_IN,
-    TOKEN_REDIRECT_OUT,
-    TOKEN_REDIRECT_APPEND,
-    TOKEN_REDIRECT_HEREDOC
-} t_token_type;
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,
+	TOKEN_REDIRECT_OUT,
+	TOKEN_REDIRECT_APPEND,
+	TOKEN_REDIRECT_HEREDOC
+}	t_token_type;
 
 typedef enum e_segment_type {
-    LITERAL,
-    VARIABLE,
-    VAR_EXIT_STATUS
-} t_segment_type;
+	LITERAL,
+	VARIABLE,
+	EXIT_STATUS
+}	t_segment_type;
 
 typedef enum e_redirect_type {
-    REDIR_IN,
-    REDIR_OUT,
-    REDIR_APPEND,
-    REDIR_HEREDOC
-} t_redirect_type;
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
+}	t_redirect_type;
 
 typedef struct s_segment {
-    t_segment_type type;
-    char *value;
-} t_segment;
+	t_segment_type	type;
+	char			*value;
+}	t_segment;
 
 typedef struct s_word {
-    t_list *segments;
-} t_word;
+	t_list			*segments;
+}	t_word;
 
 typedef struct s_token {
-    t_token_type type;
-    t_word *word;
-} t_token;
-
-typedef struct s_command {
-    char **argv;
-    t_list *redirects;  // List of t_redirect structures
-} t_command;
+	t_token_type	type;
+	t_word			*word;
+}	t_token;
 
 typedef struct s_redirect {
-    t_redirect_type type;
-    char *filename;
-} t_redirect;
+	t_redirect_type	type;
+	t_word			*filename;  // Changed from char* to t_word*
+}	t_redirect;
+
+typedef struct s_command {
+	t_list			*arguments;  // Added missing members
+	t_list			*redirects;
+}	t_command;
 
 typedef struct s_pipeline {
-    t_list *commands;    // List of t_command
-    t_list *pipes;       // Number of pipes
-} t_pipeline;
+	t_list			*commands;
+}	t_pipeline;
 
 // lexer.c
 // Token functions
