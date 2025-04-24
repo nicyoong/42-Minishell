d2b4fb013bf3559d7631de663de8fee97b617b93
nicyoong <nicyoong321@gmail.com>
2025-04-24 21:21:17 +0800

    refactor: split process quoted content
    split process_quoted_content into two by adding a function handle_variable_expansion and call it in process_quoted_content

--- src/lexer.c
+++ src/lexer.c
@@ -39,6 +39,33 @@ void free_token(void *token_ptr) {
 // ==============================
 // Lexer Logic
 // ==============================
+
+void handle_variable_expansion(const char *input, int *i, t_word *word, char *buffer, int *buf_idx) {
+    // Finalize the current literal segment if any
+    if (*buf_idx > 0) {
+        buffer[*buf_idx] = '\0';
+        add_segment(word, LITERAL, buffer);
+        *buf_idx = 0;
+    }
+    
+    // Skip the '$' character
+    (*i)++;
+    
+    if (input[*i] == '?') {  // Handle $? case
+        add_segment(word, EXIT_STATUS, "$?");
+        (*i)++;
+    } else {
+        char var[1024];
+        int var_idx = 0;
+        // Collect variable name characters
+        while (is_valid_var_char(input[*i])) {
+            var[var_idx++] = input[(*i)++];
+        }
+        var[var_idx] = '\0';
+        add_segment(word, VARIABLE, var);
+    }
+}
+
 int process_quoted_content(const char *input, int *i, char quote_type, t_word *word) {
     char buffer[1024];  // Simplified fixed buffer (use dynamic in real code)
     int buf_idx = 0;
@@ -46,26 +73,7 @@ int process_quoted_content(const char *input, int *i, char quote_type, t_word *w
     (*i)++;  // Skip opening quote
     while (input[*i] && input[*i] != quote_type) {
         if (quote_type == '"' && input[*i] == '$') {
-            // Finalize literal before variable
-            if (buf_idx > 0) {
-                buffer[buf_idx] = '\0';
-                add_segment(word, LITERAL, buffer);
-                buf_idx = 0;
-            }
-            // Process variable
-            (*i)++;
-            if (input[*i] == '?') {
-                add_segment(word, EXIT_STATUS, "$?");
-                (*i)++;
-            } else {
-                char var[1024];
-                int var_idx = 0;
-                while (is_valid_var_char(input[*i])) {
-                    var[var_idx++] = input[(*i)++];
-                }
-                var[var_idx] = '\0';
-                add_segment(word, VARIABLE, var);
-            }
+            handle_variable_expansion(input, i, word, buffer, &buf_idx);
         } else {
             buffer[buf_idx++] = input[(*i)++];
         }
