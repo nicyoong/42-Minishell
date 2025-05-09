617e1297bae551d7ef67ec7e340d05a3d1c0ee8f
nicyoong <nicyoong321@gmail.com>
2025-05-09 20:35:51 +0800

    fix: modify resolve segment function

--- src/minishell_executor.c
+++ src/minishell_executor.c
@@ -44,21 +44,14 @@ int process_heredoc(t_word *delimiter_word)
     return (fds[0]);
 }
 
-char *resolve_segment(t_segment *seg)
-{
-    if (seg->type == VARIABLE)
-    {
+char *resolve_segment(t_segment *seg, t_executor_ctx *ctx) {
+    if (seg->type == VARIABLE) {
         char *value = getenv(seg->value);
         return value ? ft_strdup(value) : ft_strdup("");
+    } else if (seg->type == EXIT_STATUS) {
+        return ft_itoa(ctx->last_exit_status);
     }
-    else if (seg->type == EXIT_STATUS)
-    {
-        return ft_itoa(g_exit_status);
-    }
-    else
-    {
-        return ft_strdup(seg->value);
-    }
+    return ft_strdup(seg->value);
 }
 
 int setup_redirections(t_list *redirects)
