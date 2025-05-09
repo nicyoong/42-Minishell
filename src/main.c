b7eca4d81734d7a338b0e361929107bafabee743
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-09 21:11:46 +0800

    Include signal handler for SIGINT, SIGQUIT, EOF
    Signal handling

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 //#include "minishell.h"
-
+#include <signals.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -65,13 +65,41 @@ int main(void)
     return (0);
 }*/
 
+void sigint_handler(int signo)
+{
+    (void)signo;
+    write(STDOUT_FILENO, "\n", 1);
+    rl_replace_line("", 0);
+    rl_on_new_line();
+    rl_redisplay();
+}
+
+void setup_signal_handlers(void)
+{
+    struct sigaction sa_int;
+    struct sigaction sa_quit;
+
+    sa_int.sa_handler = sigint_handler;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = SA_RESTART;
+    if (sigaction(SIGINT, &sa_int, NULL) == -1)
+        perror("sigaction SIGINT");
+
+    sa_quit.sa_handler = SIG_IGN;
+    sigemptyset(&sa_quit.sa_mask);
+    sa_quit.sa_flags = 0;
+    if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
+        perror("sigaction SIGQUIT");
+}
+
 int main(void)
 {
     char *line;
     t_list *tokens;
     t_pipeline *pipeline;
     t_executor_ctx ctx = {0};
-
+    setup_signal_handlers();
+	
     while (1)
 	{
         char cwd[PATH_MAX];
