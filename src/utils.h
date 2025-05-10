bfcd8f35c81493f87fc682aeffb8d7129477c50b
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-10 17:35:33 +0800

    Update utils.h - from utils2.c

--- src/utils.h
+++ src/utils.h
@@ -6,5 +6,8 @@
 int ft_strcmp(const char *s1, const char *s2);
 void ft_split_free(char **arr);
 int  is_builtin(const char *cmd);
+int is_whitespace(char c);
+int is_operator_char(char c);
+int is_valid_var_char(char c);
 
 # endif
