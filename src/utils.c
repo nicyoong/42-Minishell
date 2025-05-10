41f8bf675ace8aa0defee35d98c5a19549463913
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-10 18:23:28 +0800

    Norm'd utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,55 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tching <tching@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 18:10:31 by tching            #+#    #+#             */
+/*   Updated: 2025/05/10 18:22:34 by tching           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "minishell.h"
 #include "utils.h"
 
-int ft_strcmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return ((unsigned char)*s1 - (unsigned char)*s2);
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
-void ft_split_free(char **arr)
+void	ft_split_free(char **array)
 {
-    if (!arr)
-        return;
-    for (int i = 0; arr[i]; i++)
-        free(arr[i]);
-    free(arr);
+	int	i;
+
+	i = 0;
+	if (!array)
+		return ;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
 
-int is_builtin(const char *cmd)
+int	is_builtin(const char *cmd)
 {
-    return (cmd && (ft_strcmp(cmd, "cd") == 0 || 
-                   ft_strcmp(cmd, "export") == 0 || 
-                   ft_strcmp(cmd, "unset") == 0 ||
-                   ft_strcmp(cmd, "pwd") == 0 ||
-                   ft_strcmp(cmd, "exit") == 0));
+	return (cmd && (ft_strcmp(cmd, "cd") == 0
+			|| ft_strcmp(cmd, "export") == 0
+			|| ft_strcmp(cmd, "unset") == 0
+			|| ft_strcmp(cmd, "pwd") == 0
+			|| ft_strcmp(cmd, "exit") == 0));
 }
 
-int is_valid_identifier(const char *str)
+int	is_valid_identifier(const char *str)
 {
-    if (!str || !*str || !(ft_isalpha(*str) || *str == '_'))
-        return 0;
-    for (int i = 1; str[i]; i++) {
-        if (!ft_isalnum(str[i]) && str[i] != '_')
-            return 0;
-    }
-    return 1;
+	int	i;
+
+	i = 1;
+	if (!str || !*str || !(ft_isalpha(*str) || *str == '_'))
+		return (0);
+	while (str[i])
+	{
+		if (!ft_alnum(str[i] && str[i] != '_'))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int is_valid_integer(const char *str)
+int	is_valid_integer(const char *str)
 {
-    int i = 0;
+	int	i;
 
-    if (str[i] == '+' || str[i] == '-')
-        i++;
-    if (str[i] == '\0')
-        return 0;
-    while (str[i]) {
-        if (!ft_isdigit(str[i]))
-            return 0;
-        i++;
-    }
-    return 1;
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
