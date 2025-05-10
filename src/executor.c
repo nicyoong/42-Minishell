ece3db30f6716ccf81ebc05936a2ec1a5bf65fba
nicyoong <nicyoong321@gmail.com>
2025-05-10 19:59:21 +0800

    Update executor.c

--- src/executor.c
+++ src/executor.c
@@ -362,22 +362,21 @@ int execute_exit(char **argv, t_executor_ctx *ctx)
     int exit_code = ctx->last_exit_status;
     int arg_count = 0;
 
-    while (argv[arg_count])
-        arg_count++;
+    while (argv[arg_count]) arg_count++;
     arg_count--;
     if (arg_count > 1) {
         ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
         exit_code = 1;
     }
-    else if (arg_count == 1 && !is_valid_integer(argv[1])) {
-        ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
-        exit_code = 2;
-    }
     else if (arg_count == 1) {
-        exit_code = ft_atoi(argv[1]);
-        exit_code = exit_code % 256;
-        if (exit_code < 0)
-            exit_code += 256;
+        if (!is_valid_integer(argv[1])) {
+            ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
+            exit_code = 2;
+        } else {
+            exit_code = ft_atoi(argv[1]);
+            exit_code = exit_code % 256;
+            if (exit_code < 0) exit_code += 256;
+        }
     }
     exit(exit_code);
 }
