fa02873dfbad467d17c32a86ae0b6eeda713f64d
nicyoong <nicyoong321@gmail.com>
2025-05-10 17:16:17 +0800

    fix: update isbuiltin

--- src/utils.c
+++ src/utils.c
@@ -21,5 +21,6 @@ void ft_split_free(char **arr)
 int is_builtin(const char *cmd) {
     return (cmd && (ft_strcmp(cmd, "cd") == 0 || 
                    ft_strcmp(cmd, "export") == 0 || 
-                   ft_strcmp(cmd, "unset") == 0));
+                   ft_strcmp(cmd, "unset") == 0 ||
+                   ft_strcmp(cmd, "pwd") == 0));
 }
