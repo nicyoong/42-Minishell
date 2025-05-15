9c89dce2050976520607ed5536f69fe05ea44597
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-15 23:06:38 +0800

    ft_strncpy in minishell.h

--- src/minishell.h
+++ src/minishell.h
@@ -26,6 +26,7 @@ int is_valid_var_char(char c);
 int is_valid_identifier(const char *str);
 int is_valid_integer(const char *str);
 char *ft_strcat(char *dest, const char *src);
+char *ft_strncpy(char *dest, const char *src, size_t n);
 
 typedef enum e_token_type {
 	TOKEN_WORD,
