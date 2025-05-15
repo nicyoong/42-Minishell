2abcd83a84d1f45c0ac484f9b89e1bfd3ee6e5c1
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-15 23:05:54 +0800

    remove echo

--- src/executor.c
+++ src/executor.c
@@ -289,38 +289,6 @@ void cleanup_redirections(int save_stdin, int save_stdout, int save_stderr, t_ex
     ctx->last_exit_status = ret;
 }
 
-int execute_echo(char **argv, t_list *redirects, t_executor_ctx *ctx)
-{
-    int save_stdin = dup(STDIN_FILENO);
-    int save_stdout = dup(STDOUT_FILENO);
-    int save_stderr = dup(STDERR_FILENO);
-    int ret = 0;
-    int newline = 1;
-    int i = 1;
-
-    if (setup_redirections(redirects, ctx) < 0)
-    {
-        ret = 1;
-        cleanup_redirections(save_stdin, save_stdout, save_stderr, ctx, ret);
-        return ret;
-    }
-    if (argv[i] && strcmp(argv[i], "-n") == 0)
-    {
-        newline = 0;
-        i++;
-    }
-    while (argv[i])
-    {
-        printf("%s", argv[i]);
-        if (argv[i + 1]) printf(" ");
-        i++;
-    }
-    if (newline)
-        printf("\n");
-    cleanup_redirections(save_stdin, save_stdout, save_stderr, ctx, ret);
-    return ret;
-}
-
 int execute_env(char **argv, t_list *redirects, t_executor_ctx *ctx)
 {
     int save_stdin = dup(STDIN_FILENO);
