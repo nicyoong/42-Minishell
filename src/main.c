d303946ccd8a4798bcfbd4681d599cfafa56369a
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-16 00:18:31 +0800

    remove old int main

--- src/main.c
+++ src/main.c
@@ -6,51 +6,11 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:39:43 by tching            #+#    #+#             */
-/*   Updated: 2025/05/16 00:15:59 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/05/16 00:18:21 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
- 
-// int main(void)
-// {
-//     char *line;
-//     t_list *tokens;
-//     t_pipeline *pipeline;
-//     t_executor_ctx ctx = {0};
-//     ctx.last_exit_status = 0;
-//     setup_signal_handlers();
-//     while (1) {
-//         char cwd[PATH_MAX];
-//         getcwd(cwd, sizeof(cwd));
-//         char prompt[PATH_MAX + 16];
-//         snprintf(prompt, sizeof(prompt), "%s$ ", cwd);
-//         line = readline(prompt);
-//         if (!line) {
-//             exit(ctx.last_exit_status);
-//         }
-//         if (*line) add_history(line);
-//         tokens = lex_input(line);
-//         if (!tokens) {
-//             free(line);
-//             ctx.last_exit_status = 0;
-//             continue;
-//         }
-//         pipeline = parse(tokens);
-//         if (!pipeline) {
-//             ft_lstclear(&tokens, free_token);
-//             free(line);
-//             ctx.last_exit_status = 2;
-//             continue;
-//         }
-//         execute_pipeline(pipeline, &ctx);
-//         ft_lstclear(&tokens, free_token);
-//         free_pipeline(pipeline);
-//         free(line);
-//     }
-//     rl_clear_history();
-//     return (ctx.last_exit_status);
-// }
 
 char *read_line()
 {
