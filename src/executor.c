698f803abfa2b29176f0a633fdebfb572f10bd73
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-16 23:11:06 +0800

    remove old execute cd from executor.c

--- src/executor.c
+++ src/executor.c
@@ -310,56 +310,6 @@ char **convert_arguments(t_list *args, t_executor_ctx *ctx)
 	return argv;
 }
 
-/* to remove once execute cd nom'd
-int handle_cd(char **argv, t_list *redirects, t_executor_ctx *ctx)
-{
-	int save_stdin = dup(STDIN_FILENO);
-	int save_stdout = dup(STDOUT_FILENO);
-	int save_stderr = dup(STDERR_FILENO);
-	int ret = 0;
-
-	if (setup_redirections(redirects, ctx) < 0)
-	{
-		dup2(save_stdin, STDIN_FILENO);
-		dup2(save_stdout, STDOUT_FILENO);
-		dup2(save_stderr, STDERR_FILENO);
-		close(save_stdin);
-		close(save_stdout);
-		close(save_stderr);
-		ctx->last_exit_status = 1;
-		return 1;
-	}
-	if (!argv[1]) {
-		char *home = getenv("HOME");
-		if (!home) {
-			fprintf(stderr, "cd: HOME not set\n");
-			ret = 1;
-		} else {
-			if (chdir(home) != 0) {
-				perror("cd");
-				ret = 1;
-			}
-		}
-	} else if (argv[2]) {
-		fprintf(stderr, "cd: too many arguments\n");
-		ret = 1;
-	} else {
-		if (chdir(argv[1]) != 0) {
-			perror("cd");
-			ret = 1;
-		}
-	}
-	dup2(save_stdin, STDIN_FILENO);
-	dup2(save_stdout, STDOUT_FILENO);
-	dup2(save_stderr, STDERR_FILENO);
-	close(save_stdin);
-	close(save_stdout);
-	close(save_stderr);
-	ctx->last_exit_status = ret;
-	return ret;
-}
-*/
-
 int execute_builtin(char **argv, t_list *redirects, t_executor_ctx *ctx)
 {
 	if (ft_strcmp(argv[0], "cd") == 0) {
