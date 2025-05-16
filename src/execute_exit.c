05fc41f560828978f12c1322f8e17765fe402930
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-17 01:24:28 +0800

    execute exit refactor

--- src/execute_exit.c
+++ src/execute_exit.c
@@ -6,41 +6,54 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 17:30:57 by nyoong            #+#    #+#             */
-/*   Updated: 2025/05/17 01:21:30 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/05/17 01:24:16 by nyoong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int count_args(char **argv)
+{
+	int count = 0;
+	while (argv[count])
+		count++;
+	return count;
+}
+
+int handle_too_many_args(void)
+{
+	ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+	return 1;  // Exit code for error
+}
+
+int handle_invalid_argument(void)
+{
+	ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
+	return 2;  // Exit code for invalid numeric argument
+}
+
+int parse_exit_code(char *arg)
+{
+	int code = ft_atoi(arg);
+	code = code % 256;
+	if (code < 0)
+		code += 256;
+	return code;
+}
+
 int execute_exit(char **argv, t_executor_ctx *ctx)
 {
-	int	exit_code;
-	int	arg_count;
+	int exit_code = ctx->last_exit_status;
+	int arg_count = count_args(argv) - 1;
 
-	exit_code = ctx->last_exit_status;
-	arg_count = 0;
-	while (argv[arg_count])
-		arg_count++;
-	arg_count--;
 	if (arg_count > 1)
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		exit_code = 1;
-	}
+		exit_code = handle_too_many_args();
 	else if (arg_count == 1)
 	{
 		if (!is_valid_integer(argv[1]))
-		{
-			ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
-			exit_code = 2;
-		}
+			exit_code = handle_invalid_argument();
 		else
-		{
-			exit_code = ft_atoi(argv[1]);
-			exit_code = exit_code % 256;
-			if (exit_code < 0)
-				exit_code += 256;
-		}
+			exit_code = parse_exit_code(argv[1]);
 	}
 	exit(exit_code);
 }
