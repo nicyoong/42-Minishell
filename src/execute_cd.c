619f845ec4c7247abc84b68ed18c718c75564370
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-16 23:07:04 +0800

    nom'd execute_cd

--- src/execute_cd.c
+++ src/execute_cd.c
@@ -6,13 +6,13 @@
 /*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:44:19 by tching            #+#    #+#             */
-/*   Updated: 2025/05/16 17:33:45 by nyoong           ###   ########.fr       */
+/*   Updated: 2025/05/16 23:06:24 by tching           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void restore_std_fds(int save_stdin, int save_stdout, int save_stderr)
+static void	restore_std_fds(int save_stdin, int save_stdout, int save_stderr)
 {
 	dup2(save_stdin, STDIN_FILENO);
 	dup2(save_stdout, STDOUT_FILENO);
@@ -22,42 +22,56 @@ static void restore_std_fds(int save_stdin, int save_stdout, int save_stderr)
 	close(save_stderr);
 }
 
-int handle_cd(char **argv, t_list *redirects, t_executor_ctx *ctx)
+static int	ret_cd_error(void)
 {
-	int save_stdin = dup(STDIN_FILENO);
-	int save_stdout = dup(STDOUT_FILENO);
-	int save_stderr = dup(STDERR_FILENO);
-	int ret = 0;
+	perror("cd");
+	return (1);
+}
 
+static int	ret_arg_error(void)
+{
+	write (2, "cd: too many arguments\n", 23);
+	return (1);
+}
+
+static int	ret_no_arg(void)
+{
+	char	*home;
+
+	home = getenv("HOME");
+	if (!home)
+		write (2, "cd: HOME not set\n", 17);
+	else
+		if (chdir(home) != 0)
+			perror("cd");
+	return (1);
+}
+
+int	handle_cd(char **argv, t_list *redirects, t_executor_ctx *ctx)
+{
+	int	save_stdin;
+	int	save_stdout;
+	int	save_stderr;
+	int	ret;
+
+	save_stdin = dup(STDIN_FILENO);
+	save_stdout = dup(STDOUT_FILENO);
+	save_stderr = dup(STDERR_FILENO);
+	ret = 0;
 	if (setup_redirections(redirects, ctx) < 0)
 	{
 		restore_std_fds(save_stdin, save_stdout, save_stderr);
 		ctx->last_exit_status = 1;
-		return 1;
+		return (1);
 	}
-	if (!argv[1]) {
-		char *home = getenv("HOME");
-		if (!home) 
-		{
-			write(2,"cd: HOME not set\n", 17);
-			ret = 1;
-		} else {
-			if (chdir(home) != 0) {
-				perror("cd");
-				ret = 1;
-			}
-		}
-	} else if (argv[2]) {
-		fprintf(stderr, "cd: too many arguments\n");
-		ret = 1;
-	} else {
-		if (chdir(argv[1]) != 0) {
-			perror("cd");
-			ret = 1;
-		}
-	}
+	if (!argv[1])
+		ret = ret_no_arg();
+	else if (argv[2])
+		ret = ret_arg_error();
+	else
+		if (chdir(argv[1]) != 0)
+			ret = ret_cd_error();
 	restore_std_fds(save_stdin, save_stdout, save_stderr);
 	ctx->last_exit_status = ret;
-	return ret;
+	return (ret);
 }
-
