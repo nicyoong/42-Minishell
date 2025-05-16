0b320fec5249ad5320c738773854297cb4d98411
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-17 00:58:46 +0800

    process_heredoc, read_until_delimiter, resolve_delimiter_word nom'd

---
+++ src/process_heredoc.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_heredoc.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tching <tching@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 00:51:38 by tching            #+#    #+#             */
+/*   Updated: 2025/05/17 00:56:45 by tching           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*resolve_value(char *resolved, t_segment *s, t_executor_ctx *ctx)
+{
+	if (s->type == VARIABLE)
+	{
+		resolved = getenv(s->value);
+		if (!resolved)
+			return ("");
+		else if (s->type == EXIT_STATUS)
+			return (ft_itoa(ctx->last_exit_status));
+		else
+			return (s->value);
+	}
+}
+
+char	*resolve_delimiter_word(t_word *delimiter_word, t_executor_ctx *ctx)
+{
+	char		buffer[1024];
+	char		*resolved;
+	t_list		*seg;
+	t_segment	*s;
+
+	ft_memset(buffer, 0, sizeof(buffer));
+	seg = delimiter_word->segments;
+	while (seg)
+	{
+		s = seg->content;
+		resolved = NULL;
+		resolved = resolve_value(resolved, s, ctx);
+		ft_strcat(buffer, resolved);
+		if (s->type == EXIT_STATUS)
+			free(resolved);
+		seg = seg->next;
+	}
+	return (ft_strdup(buffer));
+}
+
+void	read_until_delimiter(const char *delim, int fd_write)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || strcmp(line, delim) == 0)
+		{
+			if (line)
+				free(line);
+			break ;
+		}
+		write(fd_write, line, strlen(line));
+		write(fd_write, "\n", 1);
+		free(line);
+	}
+}
+
+int	process_heredoc(t_word *delimiter_word, t_executor_ctx *ctx)
+{
+	int		fds[2];
+	char	*delim;
+
+	pipe(fds);
+	delim = resolve_delimiter_word(delimiter_word, ctx);
+	read_until_delimiter(delim, fds[1]);
+	free(delim);
+	close(fds[1]);
+	return (fds[0]);
+}
