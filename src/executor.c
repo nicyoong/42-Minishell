58ab83d1918de7dcc1a689ff18b9adab14cad210
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-17 18:24:11 +0800

    small fix

--- src/executor.c
+++ src/executor.c
@@ -184,7 +184,7 @@ void execute_child(t_command *cmd, t_executor_ctx *ctx)
 	char **argv = convert_arguments(cmd->arguments, ctx);
 
 	handle_invalid_arguments(argv);
-	(argv, cmd, ctx);
+	handle_builtin_command(argv, cmd, ctx);
 
 	char *path = resolve_binary(argv[0]);
 	handle_path_errors(path, argv);
