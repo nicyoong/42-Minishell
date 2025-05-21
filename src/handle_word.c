55bdbe5733fcb1bb1b350321cdef2cb3dcc27c0c
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-21 13:42:13 +0800

    42 header

--- src/handle_word.c
+++ src/handle_word.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   handle_word.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nyoong <nyoong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 00:17:07 by nyoong            #+#    #+#             */
 /*   Updated: 2025/05/20 00:17:47 by nyoong           ###   ########.fr       */
