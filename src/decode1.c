092781c197fa1a8fafb35f4fa7a60e50fb9b11f0
Tiara <64247886+tiarabytes@users.noreply.github.com>
2025-05-21 13:36:16 +0800

    42 header

--- src/decode1.c
+++ src/decode1.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   decode1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nyoong <nyoong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 23:17:22 by nyoong            #+#    #+#             */
 /*   Updated: 2025/05/17 23:18:55 by nyoong           ###   ########.fr       */
