e4798d7db80cd1a875460c4a7689815ac99d24f1
Nicholas Yoong <nicyoong321@gmail.com>
2025-05-27 03:09:47 +0800

    ft strcasecmp

--- srcbonus/expand_wildcard_bonus.c
+++ srcbonus/expand_wildcard_bonus.c
@@ -57,3 +57,18 @@ void free_match_node(void *content)
 {
     (void)content;  // do nothing to the token pointer
 }
+
+int ft_strcasecmp(const char *s1, const char *s2)
+{
+    unsigned char c1, c2;
+    while (*s1 || *s2)
+    {
+        c1 = (unsigned char)*s1++;
+        c2 = (unsigned char)*s2++;
+        if (c1 >= 'A' && c1 <= 'Z') c1 += 'a' - 'A';
+        if (c2 >= 'A' && c2 <= 'Z') c2 += 'a' - 'A';
+        if (c1 != c2)
+            return (int)c1 - (int)c2;
+    }
+    return 0;
+}
